bc37ade76a053fc0647c202fcc401e698ed3226f
Heiker Curiel <heikercuriel@gmail.com>
2025-04-14 09:24:14 -0400

    fix(lsp): add runtime.path to lua_ls settings

--- lua/plugins/lsp/lua_ls.lua
+++ lua/plugins/lsp/lua_ls.lua
@@ -29,6 +29,10 @@ lspconfig.lua_ls.setup({
       runtime = {
         -- Tell the language server which version of Lua you're using
         version = 'LuaJIT',
+        path = {
+          'lua/?.lua',
+          'lua/?/init.lua',
+        },
       },
       diagnostics = {
         -- Get the language server to recognize the `vim` global
