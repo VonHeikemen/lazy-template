f48dcf0dc0dc6e961a71d90eb3141197f7688483
Heiker Curiel <heikercuriel@gmail.com>
2025-09-23 08:22:43 -0400

    chore: remove apex_ls and leanls from legacy_configs

--- lua/plugins/lsp/legacy_configs.lua
+++ lua/plugins/lsp/legacy_configs.lua
@@ -2,13 +2,11 @@
 -- https://github.com/neovim/nvim-lspconfig/issues/3705
 
 return {
-  'apex_ls',
   'bqnlsp',
   'cadence',
   'codeqlls',
   'drools_lsp',
   'haxe_language_server',
-  'leanls',
   'openedge_ls',
   'relay_lsp',
   'sourcery',
