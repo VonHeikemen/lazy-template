0ff72760290c78d23463ecc1a1aa91724c063b4b
Heiker Curiel <heikercuriel@gmail.com>
2025-10-19 19:54:20 -0400

    feat: refactor treesitter setup for nvim v0.11

--- lua/plugins/nvim-011/treesitter.lua
+++ lua/plugins/nvim-011/treesitter.lua
@@ -2,7 +2,6 @@
 -- https://github.com/tree-sitter/tree-sitter
 
 local Plugin = {'nvim-treesitter/nvim-treesitter'}
-local user = {}
 
 Plugin.branch = 'main'
 Plugin.build = ':TSUpdate'
@@ -12,31 +11,36 @@ function Plugin.config()
   -- NOTE: the list of supported parsers is the documentation:
   -- https://github.com/nvim-treesitter/nvim-treesitter/blob/main/SUPPORTED_LANGUAGES.md
   local parsers = {'lua', 'vim', 'vimdoc'}
-
-  local filetypes = user.map_filetypes(parsers)
   local group = vim.api.nvim_create_augroup('treesitter_cmds', {clear = true})
+
+  local ts = vim.treesitter
+  local filetypes = vim.iter(parsers)
+    :map(ts.language.get_filetypes)
+    :flatten()
+    :fold({}, function(tbl, v)
+      tbl[v] = true
+      return tbl
+    end)
 
   require('nvim-treesitter').install(parsers)
 
   vim.api.nvim_create_autocmd('FileType', {
-    pattern = filetypes,
     group = group,
-    desc = 'enable treesitter syntax highlight',
-    callback = function()
-      vim.treesitter.start()
+    desc = 'enable treesitter',
+    callback = function(event)
+      local ft = event.match
+      if filetypes[ft] == nil then
+        return
+      end
+
+      local lang = ts.language.get_lang(ft)
+      local ok, hl = pcall(ts.query.get, lang, 'highlights')
+
+      if ok and hl then
+        ts.start(event.buf, lang)
+      end
     end,
   })
-end
-
-function user.map_filetypes(parsers)
-  local result = {}
-  local get_filetypes = vim.treesitter.language.get_filetypes
-
-  for _, parser in ipairs(parsers) do
-    vim.list_extend(result, get_filetypes(parser))
-  end
-
-  return result
 end
 
 return Plugin
